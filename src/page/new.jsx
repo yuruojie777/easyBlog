b90802897bde999cd0cc16cb0e592adb99de3f74
Roger <yuruojie777@163.com>
2023-04-10 09:40:30 +1000

    update New section(delete bootstrap style)

--- src/page/new.jsx
+++ src/page/new.jsx
@@ -1,36 +1,31 @@
 import React from 'react'
 import '../css/new.css'
-import {Carousel} from "antd";
-
+import { Col, Row } from 'antd';
 export const New = () => {
 
+    const videoList = [
+        "https://www.youtube.com/embed/yN7ypxC7838",
+        "https://www.youtube.com/embed/QdHvS0D1zAI",
+        "https://www.youtube.com/embed/iL_oZGHLHvU",
+        "https://www.youtube.com/embed/8J88mIH_SDU"
+    ]
 
   return (
     <div>
-        <div className="ratio ratio-16x9">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/QdHvS0D1zAI"
-                    title="YouTube video player" frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    allowFullScreen></iframe>
-        </div>
-        <div className="ratio ratio-16x9">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/iL_oZGHLHvU"
-                    title="YouTube video player" frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    allowFullScreen></iframe>
-        </div>
-        <div className="ratio ratio-16x9">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/8J88mIH_SDU"
-                    title="YouTube video player" frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    allowFullScreen></iframe>
-        </div>
-        <div className="ratio ratio-16x9">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/yN7ypxC7838"
-                    title="YouTube video player" frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    allowFullScreen></iframe>
-        </div>
+        {
+            videoList.map((item) => {
+                return (
+                    <Row>
+                        <Col span={24}>
+                            <iframe width="100%" height="500" src={item}
+                                    title="YouTube video player" frameBorder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                    allowFullScreen></iframe>
+                        </Col>
+                    </Row>
+                )
+            })
+        }
     </div>
   )
 }
